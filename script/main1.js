ad5fbda07c36e326d65148858c58e1b5ec3763bb
harsh <50626505+Dare101@users.noreply.github.com>
2020-09-22 01:17:18 +0530

    Add files via upload

---
+++ script/main1.js
@@ -0,0 +1,27 @@
+$('.owl-carousel').owlCarousel({
+    
+    margin:10,
+    responsiveClass:true,
+    nav: true,
+        dots: true,
+      
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:4,
+            nav:true,
+            loop:true
+        }
+    }
+})
+
+
+
+
